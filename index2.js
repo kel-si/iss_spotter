069ca261f83d9f9cd4181dc4e94568ea415cdd41
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-17 23:27:37 -0700

    refactor with promises

---
+++ index2.js
@@ -0,0 +1,21 @@
+const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`The next pass is at ${datetime} for ${duration} seconds!!🛰`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("Something is wrong: ", error.message)
+  });
